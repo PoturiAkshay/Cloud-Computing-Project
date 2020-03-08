449fc4cec382de4a128c05e15d55ba31408edf45
Gaurav <gauravanand58@gmail.com>
2020-03-08 14:30:47 -0300

    Revert "[proj5409] Adding initial mobile app code with basic search"
    This reverts commit 6e4763f1bf4aac4bc60545e847d7b9fdd2170130.

--- front_end/src/Service.js
+++ front_end/src/Service.js
@@ -2,11 +2,10 @@ import axios from "axios";
 
 /*This file contains all the rest api requests */
 
-const base_url = "http://192.168.1.104:5000/";
+const base_url = "http://127.0.0.1:5000/";
 
 class Service {
   getLocationData = location => {
-  	console.log('I was triggered during componentDidMount');
     return axios.get(base_url + "search/" + location);
   };
 }
